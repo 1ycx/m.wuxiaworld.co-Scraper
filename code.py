a60140598cf35914a94781393a04a5c9d908a7a7
Yahyaa Abdullah <kogam22@gmail.com>
2018-09-01 12:49:45 +0530

    Proper chapter tiltes; minor changes

--- code.py
+++ code.py
@@ -8,46 +8,46 @@ book = epub.EpubBook()
 book.set_language('en')
 
 # Enter The Novel URL Here
-novelURL =  'http://m.wuxiaworld.co/The-Strongest-System/'
+novelURL =  'http://m.wuxiaworld.co/Peerless-Martial-God/'
 novelChapURL = novelURL + 'all.html'
 while novelURL == '':
     print("Novel URL Not Provided Inside The Script.")
     novelURL = str(input("Please Enter Novel URL : "))
 print("\r\nNovel URL Set")
 
-# Get & Passing To Beautiful Soup Main Novel Page
+# Main Novel Page -- Get, Create Instance & Select
 page = req.get(novelURL)
 soup = bs(page.text, "html5lib")
-
-# Get & Passing To Beautiful Soup Novel Chapters Page
-pageChap = req.get(novelChapURL)
-so = bs(pageChap.text, "html5lib")
-
-# Seperating Parts From Soup
-chapLi = so.select_one("#chapterlist") # Using so instance of soup
-
 about = soup.select_one('.synopsisArea_detail')
 synopsis = soup.select_one('.review')
+
+# Novel Chapters Page -- Get, Create Instance & Select
+pageChap = req.get(novelChapURL)
+so = bs(pageChap.text, "html5lib")
+chapLi = so.select_one("#chapterlist")
 
 # Book Title
 title = soup.select_one('header span').text
 book.set_title(title)
 book.spine = ['nav']
 
-
-# Get All Chapter Links
+# Get Chapter links & titles
+chapTitles = []
 links = []
 for a in chapLi.findAll('a'):
     link = novelURL + a.get('href')
     links.append(link)
-del links[0]; del links[0]; del links[0]; # Remove the starting 3 links with href="#bottom"
+    chapTitles.append(a.string)
+# Remove the starting 3 links with href="#bottom" from list
+del links[2], links[1], links[0], chapTitles[2], chapTitles[1], chapTitles[0]
+print(chapTitles)
 
 # Chapter Links
 length = len(links)
 start = 0
 end = length
-print("Name = ", title)
-print("\r\nTotal No. Of Chapters = " + str(length))
+print("\r\nName = ", title)
+print("\r\nTotal No. Of Chapters = ", length)
 print("\r\nPlease Note That No. Of Chapters Shown May Not Match The Actual Numbering")
 print("Because Some Chapters Maybe Numbered As 187-A, 187-B, 187-C Although Being")
 print("3 Different HTML Pages.")
@@ -64,43 +64,51 @@ elif check == 1:
 else :
     print("Invalid Choice. All Available Chapters Will Be Downloaded.")
 
+print("\r\n")
 
-def html_gen(elem, val, tag, insert_loc=None):
-    element = soup.new_tag(elem)
-    element.string = val
-    if insert_loc == None:
-        tag.append(element)
+def html_gen(elem, soupInstance, value, tagToAppend, insert_loc=None):
+    element = soupInstance.new_tag(elem)
+    element.string = value
+    if not insert_loc:
+        tagToAppend.append(element)
     else:
-        tag.insert(insert_loc, element)
+        tagToAppend.insert(insert_loc, element)
 
-counter = 0
+counter = start - 1
 err = []
 i = 0
+
 for i in range(start, end+1):
     
     try:
-    
+        
         if i == length:
             break
         
         #####################
-        # Dowload page
+        # Get, Pass & Select
         pageIndividual = req.get(links[i])
-
-        # Pass to BS4
-        s = bs(pageIndividual.content, "html5lib")    
-        
-        #chapterTitle = "Chapter : " + str(i)
+        s = bs(pageIndividual.text, "html5lib")    
         div = s.select_one("#chaptercontent")
-        chapterTitle = div.contents[0]
 
-        # Remove anchor tags
+        #chapterTitle = "Chapter : " + str(i)
+        chapterTitle = chapTitles[i]
+        tag = s.new_tag("div")
+        html_gen("h3", s, chapterTitle, tag)
+        html_gen("hr", s, "", tag)
+        
+        # Remove href
         for a in div.select("a"):
-            a.decompose()
+            a['href'] = '#'
+
+        # Append to tag
+        tag.append(div)
+        print(tag)
+
 
         # Creates a chapter
         c2 = epub.EpubHtml(title=chapterTitle, file_name='chap_'+str(i)+'.xhtml', lang='hr')
-        c2.content = div.encode("utf-8")
+        c2.content = tag.encode("utf-8")
         book.add_item(c2)
 
 
@@ -110,8 +118,9 @@ for i in range(start, end+1):
         # Add to book ordering
         book.spine.append(c2)
 
-        print("Parsed Chapter " + str(i))
-        counter = counter + 1
+        print("Parsed Chapter", i)
+        counter += 1
+
 
     except KeyboardInterrupt as e:
         print("Keyboard Interrupt")
@@ -120,37 +129,36 @@ for i in range(start, end+1):
     
     except IndexError as e:
         print(e)
-        print("Possibly Incorrect Link For Chapter " + str(i))
-        print("Skipping Chapter " + str(i))
+        print("Possibly Incorrect Link For Chapter", i)
+        print("Skipping Chapter", i)
         err.append(i)
     
     except Exception as e:
         print(e)
         err.append(i)
 
+if counter < 0: counter = 0
+
 # About Novel
 about.find('img').decompose() # Remove IMG tags
 for a in about.select("a"):   # Remove anchor tags
-    a.decompose()
-html_gen("hr", '', about)
-html_gen("h3", "Description", about)
+    a['href'] = '#'
+html_gen("hr", soup, "", about)
+html_gen("h3", soup, "Description", about)
 syn = synopsis.text.replace("Description","")
-html_gen("p", syn, about)
-html_gen("hr", '', about)
-html_gen("h3", "About This Download : ", about)
-html_gen("p", "Total Chapters = " + str(counter), about)
-html_gen("p", "No. Of Chapters That Raised Exceptions = " + str(len(err)), about)
+html_gen("p", soup, syn, about)
+html_gen("hr", soup, "", about)
+html_gen("h3", soup, "About This Download : ", about)
+html_gen("p", soup, "Total Chapters = " + str(counter), about)
+html_gen("p", soup, "No. Of Chapters That Raised Exceptions = " + str(len(err)), about)
 if len(err) != 0:
-    html_gen("p", "And They Are : ", about)
+    html_gen("p", soup, "And They Are : ", about)
     for i in err:
-        html_gen("li", str(i), about)
-html_gen("hr", '', about)
-
-
+        html_gen("li", soup, str(i), about)
+html_gen("hr", soup, "", about)
 
 # Create About Novel Page
-aboutNovelTitle = "About Novel"
-c1 = epub.EpubHtml(title=aboutNovelTitle, file_name='About_novel'+'.xhtml', lang='hr')
+c1 = epub.EpubHtml(title="About Novel", file_name='About_novel'+'.xhtml', lang='hr')
 c1.content = about.encode('utf-8')
 book.add_item(c1)
 book.toc.insert(0, c1)
@@ -162,7 +170,7 @@ book.add_item(epub.EpubNcx())
 book.add_item(epub.EpubNav())
 
 # Defines CSS Style
-style = 'p {text-align: left;}'
+style = 'p { text-align : left; }'
 nav_css = epub.EpubItem(uid="style_nav", file_name="style/nav.css", media_type="text/css", content=style)
 
 # Adds CSS File
@@ -181,7 +189,9 @@ book.add_item(nav_css)
 # OR 
 # 2 - Move This Script To, And Run From The Location To Be Saved
 pathToLocation = ''
-saveLocation = pathToLocation + title + '_' + str(start) + '_' + str(i) + '.epub'
+downloadDetails = '"' + title + '_' + str(start) + '_' + str(counter) + '.epub"'
+saveLocation = pathToLocation + downloadDetails
+
 
 print("Saving . . .")
 
@@ -190,7 +200,7 @@ epub.write_epub(saveLocation, book, {})
 
 # Location File Got Saved
 if pathToLocation == '':
-    print("Saved at " + str(os.getcwd()) + ' as "' + title + '_' + str(start) + '_' + str(i) + '.epub"') # Example : Saved at /home/Adam/Documents/ as "The Strongest System_0_3.epub"
+    print("Saved at", os.getcwd(), 'as', downloadDetails) 
+    # Example : Saved at /home/Adam/Documents as "The Strongest System_0_3.epub"
 else :
-    print("Saved at " + saveLocation)
-
+    print("Saved at", saveLocation)
